b7a2fd6e961c99be91c54fffbf203f0a51bf461b
BJS611 <65802479+BJS611@users.noreply.github.com>
2020-10-23 08:09:55 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,9 @@
+<?php
+# Mr.Black86 Project
+# YT Crypto Gratisan
+# Thanks for your support to SUBSCRIBE
+
+// input your data
+$cfduid = "__cfduid=d65c462fe3fb13fa9fe07ece6658e159f1603367904;first_visit=2020-10-22+12%3A58%3A24;_ga=GA1.2.592727406.1603367910;_gid=GA1.2.1479139466.1603367910;_ym_uid=1603367912241348746;_ym_d=1603367912;_ym_isad=2;_ym_visorc_60756259=w;_gat_gtag_UA_160424928_1=1;__atuvc=26%7C43;__atuvs=5f92154f1de7c43c009";
+$pass   = "(++++)";
+$email  = "brojackvga@yandex.com";
